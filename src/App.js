d8cbf2d475f53b6850f28b4d0c55d52ba0ae1569
WVlada <vladimir.bukvic@live.com>
2019-11-18 18:29:21 +0100

    implemented firebase utils

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import HomePage from "./pages/homepage/homepage.component";
 
 import ShopPage from "./pages/shop/shop.component.jsx";
 
-import { auth } from "./firebase/firebase.utils";
+import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
 
 import SignInAndSignUp from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
 
@@ -23,9 +23,27 @@ class App extends React.Component {
   unsubscrubiFromAuth = null;
 
   componentDidMount() {
-    this.unsubscrubiFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({ currentUser: user });
-      console.log(user);
+    this.unsubscrubiFromAuth = auth.onAuthStateChanged(async userAuth => {
+      //this.setState({ currentUser: user });
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+        userRef.onSnapshot(snapShot => {
+          this.setState(
+            {
+              currentUser: {
+                id: snapShot.id,
+                ...snapShot.data()
+              }
+            },
+            () => {
+              console.log(this.state); // ovo znaci kada zavrsi setState
+            }
+          );
+        });
+      } else {
+        // else ako je null
+        this.setState({ currentUser: userAuth }); //ovo je null
+      }
     });
   }
 
