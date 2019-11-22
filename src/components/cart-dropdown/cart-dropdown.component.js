d5d447b929a10260939601d515036ba0655ac320
WVlada <vladimir.bukvic@live.com>
2019-11-22 16:14:15 +0100

    updated with selectors

--- src/components/cart-dropdown/cart-dropdown.component.js
+++ src/components/cart-dropdown/cart-dropdown.component.js
@@ -4,6 +4,8 @@ import "./cart-dropdown.styles.scss";
 import CartItem from "../cart-item/cart-item.componenet";
 import { connect } from "react-redux";
 
+import { selectCartItems } from "../../redux/cart/cart.selectors";
+
 const CartDropdown = ({ cartItems }) => (
   <div className="cart-dropdown">
     <div className="cart-items">
@@ -15,8 +17,8 @@ const CartDropdown = ({ cartItems }) => (
   </div>
 );
 
-const mapStateToProps = ({ cart: { cartItems } }) => ({
-  cartItems
+const mapStateToProps = state => ({
+  cartItems: selectCartItems(state)
 });
 
 export default connect(mapStateToProps)(CartDropdown);
--- src/components/cart-icon/cart-icon.component.js
+++ src/components/cart-icon/cart-icon.component.js
@@ -6,10 +6,12 @@ import { ReactComponent as ShoppingIcon } from "../../assets/shopping-bag.svg";
 import { connect } from "react-redux";
 import { toggleCartHidden } from "../../redux/cart/cart.actions";
 
-const CartIcon = ({ toggleCartHidden }) => (
+import { selectCartItemsCount } from "../../redux/cart/cart.selectors";
+
+const CartIcon = ({ toggleCartHidden, itemCount }) => (
   <div className="cart-icon" onClick={toggleCartHidden}>
     <ShoppingIcon className="shopping-icon"></ShoppingIcon>
-    <span className="item-count">0</span>
+    <span className="item-count">{itemCount}</span>
   </div>
 );
 
@@ -17,4 +19,12 @@ const mapDispatchToProps = dispatch => ({
   toggleCartHidden: () => dispatch(toggleCartHidden())
 });
 
-export default connect(null, mapDispatchToProps)(CartIcon);
+const mapStatetoProps = state => ({
+  itemCount: selectCartItemsCount(state)
+  //itemCount: cartItems.reduce(
+  //  (accumulatedQuantity, cartItem) => accumulatedQuantity + cartItem.quantity,
+  //  0
+  //)
+});
+
+export default connect(mapStatetoProps, mapDispatchToProps)(CartIcon);
